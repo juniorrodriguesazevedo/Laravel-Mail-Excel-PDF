5b58b9c71cf37af5f876823525210db680c43321
Junior Rodrigues <69818760+juniorrodriguesazevedo@users.noreply.github.com>
2021-06-10 09:48:07 -0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Admin\BookController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return redirect()->route('book.index');
 });
 
 Route::prefix('admin')->middleware(['auth', 'verified'])->group(function() {
@@ -16,9 +16,3 @@ Route::prefix('admin')->middleware(['auth', 'verified'])->group(function() {
 });
 
 Auth::routes(['verify' => true]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
